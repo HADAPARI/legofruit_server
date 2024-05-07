de690b7ecae44a1b30add8244646369789ac223f
gitAntonio24 <rakotondrabejhonnyantonio6@gmail.com>
2024-05-07 11:32:21 +0300

    stripe payment back end

---
+++ src/main/java/mg/legofruit/server/Stripe/StripeClient.java
@@ -0,0 +1,45 @@
+package mg.legofruit.server.Stripe;
+
+import com.stripe.Stripe;
+import com.stripe.model.Charge;
+import com.stripe.model.Customer;
+import org.springframework.stereotype.Component;
+
+import java.util.HashMap;
+import java.util.Map;
+
+@Component
+public class StripeClient {
+
+    public StripeClient() {
+        Stripe.apiKey = "sk_test_51PDLA2EgcFAk8EMaB3QhAX0q4wFuiyvgsC0GaRCNAh8H5vI4MdwQn81MA1YNxj4Xa82lyyJrazDHGZqFyxKeZcyc00hPiM1yzY";
+    }
+
+    public Customer createCustomer(String token, String email) throws Exception {
+        Map<String, Object> customerParams = new HashMap<String, Object>();
+        customerParams.put("email", email);
+        customerParams.put("source", token);
+        return Customer.create(customerParams);
+    }
+    private Customer getCustomer(String id) throws Exception {
+        return Customer.retrieve(id);
+    }
+    public Charge chargeNewCard(String token, double amount) throws Exception {
+        Map<String, Object> chargeParams = new HashMap<String, Object>();
+        chargeParams.put("amount", (int)(amount * 100));
+        chargeParams.put("currency", "USD");
+        chargeParams.put("source", token);
+        Charge charge = Charge.create(chargeParams);
+        return charge;
+    }
+    public Charge chargeCustomerCard(String customerId, int amount) throws Exception {
+        String sourceCard = getCustomer(customerId).getDefaultSource();
+        Map<String, Object> chargeParams = new HashMap<String, Object>();
+        chargeParams.put("amount", amount);
+        chargeParams.put("currency", "USD");
+        chargeParams.put("customer", customerId);
+        chargeParams.put("source", sourceCard);
+        Charge charge = Charge.create(chargeParams);
+        return charge;
+    }
+}
